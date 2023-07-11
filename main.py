27cf8b197f6d14a5641d49acef863f438a470645
m1jlariosm <jululama1@gmail.com>
2023-07-11 15:39:58 -0600

    Add Lines

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
-for index, row in df.iterrows():
+for index, row in df.iterrows():   # Generate the main pages
     pdf.add_page()
     pdf.set_font("Arial", "B", 24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    for y in range(20, 290, 10):     # Lines in the main pages
+        pdf.line(10, y, 200, y)
 
     pdf.ln(260)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
 
-    for i in range(row["Pages"] - 1):
+    for i in range(row["Pages"] - 1):    # Generate the secondary pages
         pdf.add_page()
         pdf.ln(272)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+
+        for y in range(20, 290, 10):     # Lines in the secondary pages
+            pdf.line(10, y, 200, y)
 
 pdf.output("pdf-file.pdf")
